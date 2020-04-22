8d980ef3c33dcc968b35145f9f2d4b6498286531
cetteup <cetteup@dasemail.de>
2020-04-22 21:27:58 +0200

    Updated BR menu option position for quads

--- controller.py
+++ controller.py
@@ -401,7 +401,7 @@ while True:
 
     # Click battle royal
     print_log('Clicking "Battle Royale"-option')
-    mouse_move(gameWindow['rect'][0] + 233, gameWindow['rect'][1] + 342)
+    mouse_move(gameWindow['rect'][0] + 233, gameWindow['rect'][1] + 210)
     mouse_left_click()
 
     time.sleep(30)
